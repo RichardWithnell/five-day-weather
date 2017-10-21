ba3b64dc162ed2e2bfb37c337c0502fd965e6755
Richard <richard.withnell@bmce-networks.com>
2017-10-21 17:16:08 +0100

    create-react-app + installed additional dependencies

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+/* @flow */
+
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>Five Day Weather</h1>
+  </div>
+);
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+/* @flow */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+import registerServiceWorker from './registerServiceWorker';
+
+const rootEl = document.getElementById('root');
+if (rootEl) {
+  ReactDOM.render(<App />, rootEl);
+}
+registerServiceWorker();
