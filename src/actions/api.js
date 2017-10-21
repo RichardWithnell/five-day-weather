1ab6ba69688c094dacf834dabbafb7835f47afd1
Richard <richard.withnell@bmce-networks.com>
2017-10-21 22:18:19 +0100

    Add actions to api.js

--- src/actions/api.js
+++ src/actions/api.js
@@ -1,6 +1,20 @@
 /* @flow */
 
+import reduceWeather from '../util'
+import data from '../data/weather.json'
+
 export const INVALIDATE_WEATHER = 'INVALIDATE_WEATHER'
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 export const FETCH_WEATHER_FULFILLED = 'FETCH_WEATHER_FULFILLED'
 export const FETCH_WEATHER_REJECTED = 'FETCH_WEATHER_REJECTED'
+
+export function invalidateWeather () {
+  return (dispatch: Function) => dispatch({ type: INVALIDATE_WEATHER })
+}
+
+export function fetchWeather () {
+  return (dispatch: Function) => {
+    dispatch({ type: FETCH_WEATHER })
+    dispatch({ type: FETCH_WEATHER_FULFILLED, payload: reduceWeather(data) })
+  }
+}
