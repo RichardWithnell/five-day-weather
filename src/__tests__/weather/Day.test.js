03803494e118afd97bf60172069582b702a02c6f
Richard <richard.withnell@bmce-networks.com>
2017-10-21 19:34:07 +0100

    Improve UI tests

--- src/__tests__/weather/Day.test.js
+++ src/__tests__/weather/Day.test.js
@@ -1,11 +1,35 @@
-/* @flow */
-
 import React from 'react'
 import { shallow } from 'enzyme'
-import { Day } from '../../components/weather'
+import { Day, Row } from '../../components/weather'
 
 describe('Test Day Component', () => {
   it('renders without crashing', () => {
     shallow(<Day />)
+  })
+
+  let props = {
+    day: '2017-01-01',
+    data: [
+      {
+        time: '13:00',
+        temp: 10.0,
+        windSpeed: 4.0,
+        image: '',
+        description: 'rain'
+      },
+      {
+        time: '14:00',
+        temp: 12.0,
+        windSpeed: 8.0,
+        image: '',
+        description: 'rain'
+      }
+    ]
+  }
+
+  it('renders props correctly', () => {
+    let day = shallow(<Day {...props} />)
+    const rows = day.find(Row)
+    expect(rows.length).toEqual(2)
   })
 })
--- src/__tests__/weather/Layout.test.js
+++ src/__tests__/weather/Layout.test.js
@@ -1,11 +1,46 @@
-/* @flow */
-
 import React from 'react'
 import { shallow } from 'enzyme'
-import { Layout } from '../../components/weather'
+import { Day, Layout } from '../../components/weather'
 
 describe('Test Layout Component', () => {
   it('renders without crashing', () => {
     shallow(<Layout />)
+  })
+
+  it('should contain no days by default', () => {
+    const layout = shallow(<Layout />)
+    const days = layout.find(Day)
+    expect(days.length).toEqual(0)
+  })
+
+  it('should contain no city/country by default', () => {
+    const layout = shallow(<Layout />)
+    const header = layout.find('h2').text()
+    expect(header).toEqual(', ')
+  })
+
+  let props = {
+    lastUpdated: new Date(),
+    country: 'UK',
+    city: 'London',
+    weather: {
+      '2017-01-01': [],
+      '2017-02-01': [],
+      '2017-03-01': [],
+      '2017-04-01': [],
+      '2017-05-01': []
+    }
+  }
+
+  it('should contain five days', () => {
+    const layout = shallow(<Layout {...props} />)
+    const days = layout.find(Day)
+    expect(days.length).toEqual(5)
+  })
+
+  it('should contain `London, UK`, when passed as a prop', () => {
+    const layout = shallow(<Layout {...props} />)
+    const header = layout.find('h2').text()
+    expect(header).toEqual('London, UK')
   })
 })
